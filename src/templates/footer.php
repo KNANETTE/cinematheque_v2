0aea7d5007b8e2161582d17676a00874a2883458
Nana KOITA <koita.nana19@gmail.com>
2026-04-03 09:44:48 +0200

    mise à jour

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,7 +1,5 @@
-<footer class="position-absolute bottom-0 start-0 end-0">
-    <p>&copy; <?= date('Y') ?> - CINEMATHEQUE</p>
-</footer>
 </body>
 <script src="/scripts/auth.js"></script>
+<script src="/scripts/movies.js"></script>
 
 </html>
--- src/templates/navbar.php
+++ src/templates/navbar.php
@@ -1,17 +1,23 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary position-fixed top-0 start-0 end-0">
     <div class="container-fluid">
         <a class="navbar-brand" href="/">CINEMATHEQUE</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Pricing</a>
-                <button class="nav-link" id="logout">Deconnexion</button>
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            <div class="navbar-nav w-75">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Genres
+                </a>
+                <ul class="dropdown-menu" id="genre-menu">
+                </ul>
+                <a class="nav-link mx-3" href="/profile">Profil</a>
+                <a class="nav-link mx-3" href="/cart">Panier</a>
+                <button class="nav-link mx-3" id="logout">Deconnexion</button>
             </div>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" id="search" type="search" placeholder="Rechercher" aria-label="Search" />
+            </form>
         </div>
     </div>
 </nav>
